421ab9fa9a38224bba3d59227e39f094d79eab88
Artem <krughlikov.rab@gmail.com>
2025-02-19 23:27:06 +0300

    fixed formatting

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,46 +1,45 @@
 <script setup>
-  import { ref } from "vue";
-  import ButtonList from './ButtonList.vue'
+import { ref } from 'vue';
+import ButtonList from './ButtonList.vue';
 
-  const displayValue = ref('')
-  const inputValue = ref('')
-  const operation = ref('')
+const displayValue = ref('');
+const inputValue = ref('');
+const operation = ref('');
 
-  const onButtonClick = (value) => {
-    if (value === 'C') {
-      displayValue.value = ''
-      inputValue.value = ''
-      operation.value = ''
-      return
-    }
-
-    if (['+', '-', 'X', '/'].includes(value)) {
-      if (inputValue.value && !['+', '-', 'X', '/'].includes(inputValue.value.slice(-1))) {
-        inputValue.value += value
-        operation.value = value
-      }
-      return;
-    }
-
-    if (value === '=' && inputValue.value) {
-      try {
-        displayValue.value = eval(inputValue.value.replace('X', '*'))
-        inputValue.value = displayValue.value.toString()
-        operation.value = ''
-      } catch {
-        displayValue.value = 'err'
-      }
+const onButtonClick = (value) => {
+  if (value === 'C') {
+    displayValue.value = '';
+    inputValue.value = '';
+    operation.value = '';
     return;
+  }
+
+  if (['+', '-', 'X', '/'].includes(value)) {
+    if (inputValue.value && !['+', '-', 'X', '/'].includes(inputValue.value.slice(-1))) {
+      inputValue.value += value;
+      operation.value = value;
     }
 
-    if (operation.value) {
-      inputValue.value += value
-      displayValue.value = value
-    } else {
-      inputValue.value += value
-      displayValue.value = inputValue.value
-    }
+    return;
   }
+
+  if (value === '=' && inputValue.value) {
+    try {
+      displayValue.value = eval(inputValue.value.replace('X', '*'));
+      inputValue.value = displayValue.value.toString();
+      operation.value = '';
+    } catch {
+      displayValue.value = 'err';
+    }
+
+    return;
+  }
+
+  inputValue.value += value;
+  displayValue.value = !operation.value
+    ? inputValue.value
+    : value;
+}
 </script>
 
 <template>
@@ -48,7 +47,10 @@
   <div class="max-w-90 mt-20 px-5 rounded-2xl bg-body-calc shadow-calc">
     <input
       v-model="displayValue"
-      class="outline-0 mt-10 mb-7 px-1 py-0.5 bg-gray-100 text-5xl w-full box-border rounded-sm text-right"
+      class="
+        outline-0 mt-10 mb-7 px-1 py-0.5 bg-gray-100
+        text-5xl w-full box-border rounded-sm text-right
+      "
     >
     <ButtonList @onButtonClick="onButtonClick"/>
   </div>
--- src/components/OneButton.vue
+++ src/components/OneButton.vue
@@ -1,11 +1,17 @@
 <script setup>
 defineProps({
   btnValue: String,
-})
+});
 </script>
 
 <template>
-<div @click="$emit('clickButton', btnValue)" class="flex justify-center bg-btn-bg rounded-xs shadow-btn active:translate-0.5 active:shadow-none">
+<div
+  class="
+    flex justify-center bg-btn-bg rounded-xs
+    shadow-btn active:translate-0.5 active:shadow-none
+  "
+  @click="$emit('clickButton', btnValue)"
+>
   <button class="text-white text-4xl leading-0 w-auto h-12">
     {{ btnValue }}
   </button>
